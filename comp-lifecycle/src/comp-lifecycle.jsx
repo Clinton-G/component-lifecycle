98dfc9c0d896dbc30db8a8c16ae8b6866927d691
Clinton.G <clintongoin@gmail.com>
2024-10-02 21:27:40 -0500

    comp-lifecycle

--- comp-lifecycle/src/comp-lifecycle.jsx
+++ comp-lifecycle/src/comp-lifecycle.jsx
@@ -1,32 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = (publicKey, hash) =>
-    'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=b9057d7e6fa0341e4244c30148cbdb25&hash=215e07d4090545867ddfa3d69f36dc58'
+const API_URL = (characterId, publicKey, hash) =>
+    `https://gateway.marvel.com/v1/public/characters/${characterId}?ts=1&apikey=${publicKey}&hash=${hash}`;
 
-const CharacterList = () => {
+    const CharacterList = () => {
     const [characters, setCharacters] = useState([]);
+    const [selectedCharacter, setSelectedCharacter] = useState(null);
 
     const PUBLIC_KEY = 'b9057d7e6fa0341e4244c30148cbdb25';
     const HASH = '215e07d4090545867ddfa3d69f36dc58';
 
     useEffect(() => {
         axios
-            .get(API_URL(PUBLIC_KEY, HASH))
+            .get(API_URL('', PUBLIC_KEY, HASH))
             .then((response) => setCharacters(response.data.data.results))
+            .catch((error) => console.error(error));
     }, [PUBLIC_KEY, HASH]);
 
+    const fetchCharacterDetails = (characterId) => {
+        axios
+            .get(API_URL(characterId, PUBLIC_KEY, HASH))
+            .then((response) => setSelectedCharacter(response.data.data.results[0]))
+            .catch((error) => console.error(error));
+    };
+
     return (
-        <div className='image-grid'>
-            {characters.map(({ id, name, thumbnail }) => (
-                <div key={id} className='grid-item'>
-                    <img
-                        src={`${thumbnail.path}.${thumbnail.extension}`}
-                        className='image'
-                    />
-                    <h3>{name}</h3>
-                </div>
-            ))}
+        <div>
+            <div className='image-grid'>
+                {characters.map(({ id, name, thumbnail }) => (
+                    <div key={id} className='grid-item' onClick={() => fetchCharacterDetails(id)}>
+                        <img
+                            src={`${thumbnail.path}.${thumbnail.extension}`}
+                            className='image'
+                        />
+                        <h3>{name}</h3>
+                    </div>
+                ))}
+            </div>
+
+            {selectedCharacter && <CharacterDetail character={selectedCharacter} />}
+        </div>
+    );
+};
+
+const CharacterDetail = ({ character }) => {
+    const { name, description, comics } = character;
+
+    return (
+        <div className='character-detail'>
+            <h2>{name}</h2>
+            <p>{description ? description : 'No description available.'}</p>
+            <h3>List of Associated Comics:</h3>
+            <ul>
+                {comics.items.map((comic, index) => (
+                    <li key={index}>{comic.name}</li>
+                ))}
+            </ul>
         </div>
     );
 };
